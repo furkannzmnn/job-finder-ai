40124706706fbe456bb274e26faa82a6ed01a4f8
furkanozmen <ozmenf97@gmail.com>
2025-02-16 14:39:19 +0300

    add .env file and enhance main.py to extract job listings in JSON format

--- main.py
+++ main.py
@@ -1,17 +1,52 @@
+import json
+import re
+import asyncio
 from langchain_google_genai import ChatGoogleGenerativeAI
 from browser_use import Agent
-import asyncio
 from dotenv import load_dotenv
+
 load_dotenv()
 
 async def main():
     search_query = "Java Developer site:linkedin.com/jobs location:Turkey"
     agent = Agent(
-        task=f"Go to Google and search '{search_query}', then extract job listings with their URLs.",
+        task=f"Go to Google and search '{search_query}', then extract job listings with their URLs. Listed maximum 5 jobs. ",
         llm=ChatGoogleGenerativeAI(model="gemini-2.0-flash-exp"),
     )
     result = await agent.run()
-    print("\n--- Job Listings ---\n")
-    print(result)
+
+    llm = ChatGoogleGenerativeAI(
+        model="gemini-2.0-flash-exp",
+        temperature=0,
+        max_tokens=None,
+        timeout=None,
+        max_retries=2,
+    )
+
+    result_str = str(result)
+    messages = [
+        (
+            "system",
+            "Please this text format: 'job title', 'company name', 'location', 'url'. And convert it to JSON format.",
+        ),
+        ("human", result_str),
+    ]
+
+    ai_message = llm.invoke(messages)
+
+    # AI mesaj içeriğini string olarak al
+    cleaned_json_str = re.sub(r'```json|```', '', ai_message.content).strip()
+
+    try:
+        json_data = json.loads(cleaned_json_str)
+
+        # JSON verisini dosyaya kaydet
+        with open("jobs.json", "w", encoding="utf-8") as json_file:
+            json.dump(json_data, json_file, ensure_ascii=False, indent=4)
+
+        print("JSON dosyası başarıyla oluşturuldu: jobs.json")
+    except json.JSONDecodeError as e:
+        print("JSON dönüşüm hatası:", e)
+
 
 asyncio.run(main())
